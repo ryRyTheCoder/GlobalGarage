3912b15273eac68395a5bf7ee210e9df62077689
“Juan_Zavala” <juan_zavala24@icloud.com>
2023-11-09 18:47:59 -0600

    Working: Create Gareage Endpoint does as described and updates respective seller

--- GlobalGarage/src/main/java/com.nashss.se.GlobalGarage/lambda/LambdaResponse.java
+++ GlobalGarage/src/main/java/com.nashss.se.GlobalGarage/lambda/LambdaResponse.java
@@ -3,6 +3,7 @@ package com.nashss.se.GlobalGarage.lambda;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -12,7 +13,8 @@ import java.util.Map;
  * An APIGateway response from a lambda function.
  */
 public class LambdaResponse extends APIGatewayProxyResponseEvent {
-    private static final ObjectMapper MAPPER = new ObjectMapper();
+    protected static final ObjectMapper MAPPER = new ObjectMapper()
+            .registerModule(new JavaTimeModule()); // Register JavaTimeModule
 
     private static final Logger log = LogManager.getLogger();
     private LambdaResponse(int statusCode, String body) {
